d9daa9f404f9fdb65f48d09aac45f02a8ebf99cc
Tobi Weinstock <tv.weinstock@gmail.com>
2017-07-15 15:19:45 -0400

    adding styles WIP, convert csv to json, and merge two jsons to integrate all retaurant details

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,65 @@ var apiKey = 'c2a03525b10a68c5cef230060122ebb4';
 var indexName = 'restaurants_list';
 
 var client = algoliasearch(applicationID, apiKey);
-var helper = algoliasearchHelper(client, indexName);
+var helper = algoliasearchHelper(client, indexName)//, {
+  // add attribute to facet in display configuration of index
+  // facets: ['food_type', 'stars_count', 'payment_options']
+// };
 
-helper.on('result', function(content) {
-  renderHits(content);
-});
+$(document).ready(function() {
 
-function renderHits(content) {
-  $('.results-list').html(function() {
-    return $.map(content.hits, function(hit) {
-      return '<li>' + hit._highlightResult.name.value + '</li>';
-    });
+  helper.on('result', function(content) {
+    // filter list for cuisine type
+    // renderFacetList(content);
+    // TODO create facet lists so users can flter by ratings and payment options
+
+    // set main list results
+    renderHits(content);
   });
-}
 
-$('#search-box').on('keyup', function() {
-  helper.setQuery($(this).val())
-        .search();
+  // render dynamic list of facets. render list each time  new results are received
+  // this list will allow user to select a value made possible using jQuery
+  function renderFacetList(content) {
+    $('#cuisine-type-list').html(function() {
+      // getFacetValues will return list of values (an object) usable to filter an attribute
+      // returned object contains 3 properties: name (facet value), count (number of items in results)
+      // isRefined (boolean - is value already selected)
+      return $.map(content.getFacetValues('food_type'), function(facet) {
+        var checkbox = $('<input type=checkbox>')
+            .data('facet', facet.name)
+            .attr('id', 'fl-' + facet.name);
+        if (facet.isRefined) checkbox.attr('checked', 'checked');
+        var label = $('label').html(facet.name + ' (' + facet.count + ')')
+                              .attr('for', 'fl-' + facet.name);
+        // dynamically append list item for each value in the results
+        return $('<li>').append(checkbox).append(label);
+
+      });
+    });
+  }
+
+  function renderHits(content) {
+    console.log(content.hits);
+    $('.results-list').html(function() {
+      return $.map(content.hits, function(hit) {
+        return '<li><img src=' + hit.image_url +'><h3>' + hit._highlightResult.name.value + '</h3></li>';
+      });
+    });
+  }
+
+  // EVENT LISTENERS
+
+  // event listener for user click on facet list
+  $('#cuisine-type-list').on('click', 'input[type=checkbox]', function(e) {
+    var facetValue = $(this).data('facet');
+    helper.toggleFacetRefinement('food_type', facetValue)
+          .search();
+  });
+
+  // event listener for user typing input
+  $('#search-box').on('keyup', function() {
+    helper.setQuery($(this).val())
+    .search();
+  });
+  helper.search();
 });
---
+++ resources/dataset/merge.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var restaurantInfo1 = require('./restaurants_list.json');
+var restaurantInfo2 = require('./restaurants_info_converted.json');
+
+var restaurantsById = {};
+restaurantInfo1.forEach(function(restaurant) {
+  restaurantsById[restaurant.objectID] = restaurant;
+});
+
+restaurantInfo2.forEach(function(restaurant) {
+  // merge object from right to left with Object.assign,
+  // pass in empty object just in case there is a new id that didn't exist in the first JSON
+  restaurantsById[restaurant.objectID] = Object.assign({}, restaurantsById[restaurant.objectID], restaurant);
+});
+
+// create array with object
+// map makes an array of keys into an array of values
+var outputArray = Object.keys(restaurantsById).map(function(key) {
+  return restaurantsById[key];
+});
+
+fs.writeFileSync('restaurant_info_merged.json', JSON.stringify(outputArray));
